d10ea1ab21418794c7bd781c93a61e2c7a0ac4be
cjm96 <cjm96@users.noreply.github.com>
2024-03-22 09:35:15 +0000

    Update affine_invariant_utils.py

--- PySO/affine_invariant_utils.py
+++ PySO/affine_invariant_utils.py
@@ -67,6 +67,10 @@ def sample_g(size=None, a=2.):
     $$ Var[z] = \frac{(a-1)^2 (a (4 a+7)+4)}{45 a^2} $$
     respectively.
 
+    Because of the mild ~z^{-1/2} dependence of the PDF the PDF is 
+    relatively flat across its entire range of support; the ratio of the
+    maximum to the minimum value of the PDF across this range is a.
+
     Choosing a larger value of the scale parameter increases both the 
     mean and the range of support of the distribution, thereby making it
     more likely that a particle will propose a large step outside of the 
